e3578028cc0e1d1c21b2eaf601bd2ca1cccd557d
Dammyjay <dammykirchhoff@gmail.com>
2025-05-20 00:35:53 +0100

    test3

--- server.js
+++ server.js
@@ -601,12 +601,10 @@ createTableIfNotExists();
 
 async function createtest2IfNotExists() {
   const query = `
-      CREATE TABLE IF NOT EXISTS test2 (
+      CREATE TABLE IF NOT EXISTS test3 (
           id SERIAL PRIMARY KEY,
-          temperature FLOAT NOT NULL,
-          humidity FLOAT NOT NULL,
-          date DATE NOT NULL,
-          time TIME NOT NULL
+          username VARCHAR(255) UNIQUE NOT NULL,
+          password VARCHAR(255) NOT NULL,
       );
   `;
   try {
